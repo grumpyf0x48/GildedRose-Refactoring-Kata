6a9eb382faeefa49d75b28bbb06085efc08a4a70
Pierre-Yves Fourmond <pierre-yves.fourmond@octo.com>
2026-03-31 08:48:56 +0200

    TDD01: Première journée de formation

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,5 +1,7 @@
 package com.gildedrose;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,11 +9,195 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
+    void quand_une_journee_passe_le_sellIn_et_la_qualite_diminue_de_1() {
+        // given
+        Item item = new Item("foo", 6, 26);
+        GildedRose app = new GildedRose(new Item[]{item});
+
+        // when
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+
+        // then
+        assertEquals(5, item.sellIn);
+        assertEquals(25, item.quality);
     }
 
+    @Test
+    void quand_une_journee_passe_et_que_la_qualite_est_a_0_le_sellIn_diminue_et_la_qualite_est_inchangee() {
+        // given
+        Item item = new Item("foo", 6, 0);
+        GildedRose app = new GildedRose(new Item[]{item});
+
+        // when
+        app.updateQuality();
+
+        // then
+        assertEquals(5, item.sellIn);
+        assertEquals(0, item.quality);
+    }
+
+    @Test
+    @DisplayName("Etant donné que le sellIn est à 0" +
+        "quand une journée passe, alors le sellIn diminue de 1" +
+        "et la qualité diminue de 2")
+    void quand_une_journee_passe_et_que_le_sellIn_est_a_0_le_sellIn_diminue_de_1_et_la_qualite_diminue_de_2() {
+        // given
+        Item item = new Item("foo", 0, 26);
+        GildedRose app = new GildedRose(new Item[]{item});
+
+        // when
+        app.updateQuality();
+
+        // then
+        assertEquals(-1, item.sellIn);
+        assertEquals(24, item.quality);
+    }
+
+    @Nested
+    public final class AgedBrieTest {
+
+        @DisplayName("Etant donné un item AgedBrie avec un sellIn positif" +
+            "quand une journée passe, alors le sellIn diminue de 1" +
+            "et la qualité augmente de 1")
+        @Test
+        void quand_une_journee_passe_le_sellIn_diminue_de_1_et_la_qualité_augmente_de_1() {
+            // given
+            Item item = new Item("Aged Brie", 6, 26);
+            GildedRose app = new GildedRose(new Item[]{item});
+
+            // when
+            app.updateQuality();
+
+            // then
+            assertEquals(5, item.sellIn);
+            assertEquals(27, item.quality);
+        }
+
+        @DisplayName("Etant donné un item AgedBrie avec un sellIn positif et une qualité de 50" +
+            "quand une journée passe, alors le sellIn diminue de 1" +
+            "et la qualité reste à 50")
+        @Test
+        void quand_une_journee_passe_le_sellIn_diminue_de_1_et_la_qualité_reste_a_50() {
+            // given
+            Item item = new Item("Aged Brie", 6, 50);
+            GildedRose app = new GildedRose(new Item[]{item});
+
+            // when
+            app.updateQuality();
+
+            // then
+            assertEquals(5, item.sellIn);
+            assertEquals(50, item.quality);
+        }
+
+        @DisplayName("Etant donné un item AgedBrie avec un sellIn à 0 et une qualité de 40" +
+            "quand une journée passe, alors le sellIn diminue de 1" +
+            "et la qualité augmente de 2")
+        @Test
+        void quand_une_journee_passe_le_sellIn_diminue_de_1_et_la_qualite_augmente_de_2() {
+            // given
+            Item item = new Item("Aged Brie", 0, 40);
+            GildedRose app = new GildedRose(new Item[]{item});
+
+            // when
+            app.updateQuality();
+
+            // then
+            assertEquals(-1, item.sellIn);
+            assertEquals(42, item.quality);
+        }
+    }
+
+    @Nested
+    public final class SulfurasTest {
+
+        @DisplayName("Etant donné un item Sulfuras avec un sellIn à 0 et une qualité de 80" +
+            "quand une journée passe, alors le sellIn reste inchangé" +
+            "et la qualité reste inchangé")
+        @Test
+        void quand_une_journee_passe_le_sellIn_reste_inchange_et_la_qualite_reste_inchange() {
+            // given
+            Item item = new Item("Sulfuras, Hand of Ragnaros", 0, 80);
+            GildedRose app = new GildedRose(new Item[]{item});
+
+            // when
+            app.updateQuality();
+
+            // then
+            assertEquals(0, item.sellIn);
+            assertEquals(80, item.quality);
+        }
+    }
+
+    @Nested
+    public final class BackstageTest {
+
+        @DisplayName("Etant donné un item Backstage avec un sellIn à 11 et une qualité de 10" +
+            "quand une journée passe, alors le sellIn diminue de 1" +
+            "et la qualité augmente de 1")
+        @Test
+        void quand_une_journee_passe_le_sellIn_diminue_de_1_et_la_qualite_augmente_de_1() {
+            // given
+            Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 11, 10);
+            GildedRose app = new GildedRose(new Item[]{item});
+
+            // when
+            app.updateQuality();
+
+            // then
+            assertEquals(10, item.sellIn);
+            assertEquals(11, item.quality);
+        }
+
+        @DisplayName("Etant donné un item Backstage avec un sellIn à 6 et une qualité de 10" +
+            "quand une journée passe, alors le sellIn diminue de 1" +
+            "et la qualité augmente de 2")
+        @Test
+        void quand_une_journee_passe_le_sellIn_diminue_de_1_et_la_qualite_augmente_de_2() {
+            // given
+            Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 6, 10);
+            GildedRose app = new GildedRose(new Item[]{item});
+
+            // when
+            app.updateQuality();
+
+            // then
+            assertEquals(5, item.sellIn);
+            assertEquals(12, item.quality);
+        }
+
+        @DisplayName("Etant donné un item Backstage avec un sellIn à 5 et une qualité de 10" +
+            "quand une journée passe, alors le sellIn diminue de 1" +
+            "et la qualité augmente de 3")
+        @Test
+        void quand_une_journee_passe_le_sellIn_diminue_de_1_et_la_qualite_augmente_de_3() {
+            // given
+            Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 10);
+            GildedRose app = new GildedRose(new Item[]{item});
+
+            // when
+            app.updateQuality();
+
+            // then
+            assertEquals(4, item.sellIn);
+            assertEquals(13, item.quality);
+        }
+
+        @DisplayName("Etant donné un item Backstage avec un sellIn à 0 et une qualité de 10" +
+            "quand une journée passe, alors le sellIn diminue de 1" +
+            "et la qualité tombe à 0")
+        @Test
+        void quand_une_journee_passe_le_sellIn_diminue_de_1_et_la_qualite_tombe_a_0() {
+            // given
+            Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10);
+            GildedRose app = new GildedRose(new Item[]{item});
+
+            // when
+            app.updateQuality();
+
+            // then
+            assertEquals(-1, item.sellIn);
+            assertEquals(0, item.quality);
+        }
+    }
 }
